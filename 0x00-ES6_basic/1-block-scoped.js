ddb0cd44c0f0789e2938b33c5a8d597f3fd6b7fd
Ahortu901 <aricktech.edu@gmail.com>
2023-01-23 23:27:07 +0000

    updated

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,4 +1,4 @@
-export default function taskBlock(trueOrFalse) {
+  export default function taskBlock(trueOrFalse) {
     const task = false;
     const task2 = true;
   
@@ -9,4 +9,3 @@ export default function taskBlock(trueOrFalse) {
   
     return [task, task2];
   }
-  
