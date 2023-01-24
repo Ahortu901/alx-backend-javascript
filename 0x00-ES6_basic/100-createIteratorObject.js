3d17173670ec4dfcc5edec4d9aedb7d461dc8852
Ahortu901 <aricktech.edu@gmail.com>
2023-01-24 00:37:40 +0000

    updated

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,5 +1,9 @@
 export default function createIteratorObject(report) {
-  while (report) {
-    console.log('Placeholder');
-  }
+  return (function* _() {
+    for (const department of Object.values(report.allEmployees)) {
+      for (const employee of department) {
+        yield employee;
+      }
+    }
+  }());
 }
--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,5 +1,9 @@
 export default function iterateThroughObject(reportWithIterator) {
-  if (!reportWithIterator) {
-    console.log('Placeholder');
+  const employees = [];
+
+  for (const employee of reportWithIterator) {
+    employees.push(employee);
   }
+
+  return employees.join(' | ');
 }
