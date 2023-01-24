3ac1bb8472599b4deb1809a24e59fe8020fb0971
Ahortu901 <aricktech.edu@gmail.com>
2023-01-24 00:18:25 +0000

    updated

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,3 +1,34 @@
 export default function createIteratorObject(report) {
-  return
+  let departments = Object.entries(report);
+  let index = 0;
+  return {
+      next: function() {
+          if (index < departments.length) {
+              let department = departments[index][1];
+              let employees = Object.entries(department);
+              let empIndex = 0;
+              index++;
+              return {
+                  nextEmp: function() {
+                      if (empIndex < employees.length) {
+                          let employee = employees[empIndex];
+                          empIndex++;
+                          return {
+                              value: employee,
+                              done: false
+                          }
+                      } else {
+                          return {
+                              done: true
+                          }
+                      }
+                  }
+              }
+          } else {
+              return {
+                  done: true
+              }
+          }
+      }
+  }
 }
