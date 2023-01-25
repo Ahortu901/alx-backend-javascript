3d37673ec79aea80e1a01cd687fe2b42877a4dee
Ahortu901 <aricktech.edu@gmail.com>
2023-01-25 10:34:40 +0000

    updated all

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,6 +1,4 @@
+/* eslint-disable */
 export default function getResponseFromAPI() {
-  /* eslint-disable no-unused-vars */
-  return new Promise((resolve, reject) => {
-    resolve(true);
-  });
+  return new Promise(() => {});
 }
