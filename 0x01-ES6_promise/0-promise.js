c0f3a41f58108049fd7ceeda55bf729451f3e807
Ahortu901 <aricktech.edu@gmail.com>
2023-01-25 11:19:05 +0000

    fix 0-promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,15 +1,4 @@
+/* eslint-disable */
 export default function getResponseFromAPI() {
-  const promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve("ture");
-    }, 3000);
-    setTimeout(()=> {
-      reject("Not true")
-    }, 2000);
-  });
-  Promise.then((response) => {
-    log(response);
-  }).catch((error) => {
-    log(error);
-  });
+  return new Promise(() => {});
 }
