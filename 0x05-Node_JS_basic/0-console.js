e43b9bf155b40d5be67bb9d89e06cb5c275ebf0e
Derrick Ahortu <116677585+Ahortu901@users.noreply.github.com>
2023-03-20 20:41:23 +0000

    updates and adding config files

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,7 +1,7 @@
 /**
  * Displays a message on the STDOUT.
  * @param {String} msg The message to display.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ *  @author Ahortu Derrick <https://github.com/Ahortu90>
  */
 const displayMessage = (msg) => {
   console.log(msg);
--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 /**
  * Counts the students in a CSV data file.
  * @param {String} dataPath The path to the CSV data file.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ *  @author Ahortu Derrick <https://github.com/Ahortu90>
  */
 const countStudents = (dataPath) => {
   if (!fs.existsSync(dataPath)) {
